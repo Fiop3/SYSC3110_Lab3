c1e2ba6d6fee911434e51fad404d98f58d0fef39
Mofiopefoluwa Olatunji <mofiolatunji@hotmail.com>
2023-09-18 12:51:41 -0400

    Changed AddressBook.java by adding addBuddy and RemoveBuddy methods

--- AddressBook.java
+++ AddressBook.java
@@ -4,13 +4,14 @@ public class AddressBook {
     private ArrayList<BuddyInfo> buddyInfos = new ArrayList<BuddyInfo>();
 
 
+
     public void addBuddy( BuddyInfo buddy ){
         buddyInfos.add(buddy);
 
 
     }
-    public void removeBuddy(BuddyInfo BuddyToRem){
-       buddyInfos.remove(BuddyToRem);
+    public void removeBuddy(BuddyInfo buddyToRem){
+       buddyInfos.remove(buddyToRem);
         }
 
     public static void main(String[] args) {
